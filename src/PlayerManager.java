bbef798a11411d3c8a731d15d7f1e888565700bb
Laboratório de Redes <labredes@Labredes.pucrs.br>
2019-09-02 22:32:15 -0300

    Fim de aula 02 9 19

---
+++ src/PlayerManager.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+
+public class PlayerManager {
+	
+	private ArrayList<Player> players;
+	
+	public void GerenciadorDeJogadores(){
+	}
+	
+	public boolean login(Player player){
+		
+		if(players.stream().anyMatch(p -> p.)
+		
+	}
+
+}
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -4,30 +4,35 @@
 
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
 
 class UDPServer {
-   public static void main(String args[])  throws Exception
-      {
-         // cria socket do servidor com a porta 9876
-         DatagramSocket serverSocket = new DatagramSocket(8080);
+	private PlayerManager playerManager;
+	
+	public static void main(String args[])  throws Exception{
+	   
+	   // cria socket do servidor com a porta 9876
+       DatagramSocket serverSocket = new DatagramSocket(8080);
 
-            byte[] receiveData = new byte[1024];
-            while(true) {
-            	System.out.println("Servidor UDP ouvindo... (fala que eu te escuto)");            	
+       byte[] receiveData = new byte[1024];
+       
+       while(true) {
+    	   System.out.println("Servidor UDP ouvindo... (fala que eu te escuto)");            	
             
-            	// declara o pacote a ser recebido
-            	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+           //declara o pacote a ser recebido
+           DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-            	//recebe o pacote do cliente
-            	serverSocket.receive(receivePacket);
+           //recebe o pacote do cliente
+           serverSocket.receive(receivePacket);
 
-            	// pega os dados, o endereçoo IP e a porta do cliente
-            	// para poder mandar a msg de volta
-            	String sentence = new String(receivePacket.getData());
-            	InetAddress IPAddress = receivePacket.getAddress();
-            	int port = receivePacket.getPort();
+           //pega os dados, o endereçoo IP e a porta do cliente
+           //para poder mandar a msg de volta
+           String sentence = new String(receivePacket.getData());
+           InetAddress IPAddress = receivePacket.getAddress();
+           int port = receivePacket.getPort();
 
-            	System.out.println("Mensagem recebida: \n" + sentence);
-           }
-      }
+           System.out.println("Mensagem recebida: \n" + sentence);
+       }
+   }
+   
 }
