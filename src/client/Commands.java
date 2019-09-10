575fd01ada871dd33cce16cb9902c236ce18a3f5
Gabriel Brunichaki <31492951+gsbrunichaki@users.noreply.github.com>
2019-09-09 22:26:17 -0300

    Begin of user interface

---
+++ src/client/Commands.java
@@ -0,0 +1,14 @@
+package client;
+
+import common.Communicates;
+
+public class Commands {
+	
+	public boolean sendLogin(String username) {
+		
+		System.out.println("O usuário " + username + " entrou no jogo.");
+		return true;
+	}
+	
+
+}
