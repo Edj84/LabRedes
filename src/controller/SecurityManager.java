a44e596a1b09d193e1127b9c9101298cf3b60eae
lsteinp <lucas.cerqueira@acad.pucrs.br>
2019-11-04 19:47:22 -0200

    changing crc calculation

--- src/controller/SecurityManager.java
+++ src/controller/SecurityManager.java
@@ -14,23 +14,18 @@ public class SecurityManager {
 		return false;
 	}
 
-	public int calculate_crc(String message) {
-		byte[] bytes = message.getBytes();
-		int i;
-		int crc_value = 0;
-		for (int len = 0; len < bytes.length; len++) {
-			for (i = 0x80; i != 0; i >>= 1) {
-				if ((crc_value & 0x8000) != 0) {
-					crc_value = (crc_value << 1) ^ 0x8005;
-				} else {
-					crc_value = crc_value << 1;
-				}
-				if ((bytes[len] & i) != 0) {
-					crc_value ^= 0x8005;
-				}
-			}
-		}
-		return crc_value;
+	public int crc16(String message) {
+	int crc = 0xFFFF;
+	byte[] buffer = message.getBytes();
+	for (int j = 0; j < buffer.length ; j++) {
+		crc = ((crc  >>> 8) | (crc  << 8) )& 0xffff;
+		crc ^= (buffer[j] & 0xff);//byte to int, trunc sign
+		crc ^= ((crc & 0xff) >> 4);
+		crc ^= (crc << 12) & 0xffff;
+		crc ^= ((crc & 0xFF) << 5) & 0xffff;
+	}
+	crc &= 0xffff;
+	return crc;
 	}
 
 	public String generateCrcError (String CRC) {
